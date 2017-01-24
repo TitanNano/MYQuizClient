035da9c4417088d91fbc3cbbc2b1648f9ce8c2ab
mike <m@noeimail.com>
2017-01-24 09:33:00 +0100

    #18 Implement the Question View in XAML

---
+++ MYQuizClient/Fragenseite.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MYQuizClient
+{
+    public partial class Fragenseite : ContentPage
+    {
+        public Fragenseite()
+        {
+            InitializeComponent();
+        }
+    }
+}
