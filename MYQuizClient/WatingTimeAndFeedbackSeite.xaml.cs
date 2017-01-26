20acb4afb9f27c1642459a27d6c9dda4600de9b9
mike <m@noeimail.com>
2017-01-26 12:27:52 +0100

    #30 - Implement UserFeedback und WaitingTime: merge

---
+++ MYQuizClient/WatingTimeAndFeedbackSeite.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MYQuizClient
+{
+    public partial class WatingTimeAndFeedbackSeite : ContentPage
+    {
+        public WatingTimeAndFeedbackSeite()
+        {
+            InitializeComponent();
+        }
+    }
+}
