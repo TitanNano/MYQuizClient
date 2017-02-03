48ddfa4912cb9c9492ec00d461db772359a3448c
mike <m@noeimail.com>
2017-02-03 15:07:06 +0100

    #42 - Implement enter a group: logic in LoginView added

--- MYQuizClient/Views/LoginView.xaml.cs
+++ MYQuizClient/Views/LoginView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MYQuizClient.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,48 @@ namespace MYQuizClient
 
         public void entry_Completed(object sender, EventArgs e)
         {
-            App.navigateTo(App.questionView);
+            enterGroup();
         }
+
+
+        public async void enterGroup()
+        {
+            try {
+                //Nur wenn Device registriert ist, hat es eine ClientId,
+                //dann darf man erst die Gruppe betreten.
+                if (Settings.ClientId != String.Empty)
+                {
+                    string groupPin = en_pincode.Text;
+                    var group = await Networking.Current.enterGroup(groupPin);
+
+                    //Wenn als Antwort vom Server die Gruppe zurückgeliefert wird,
+                    //darf ich zur QuestionView
+                    if (group != null)
+                    {
+                        App.navigateTo(App.questionView);
+                    }
+                }else
+                {
+                    //Falls Registrierung beim App.OnStart() misslingt
+                    //hier nochmal neu registrieren:
+                    await Networking.Current.registerClientDevice();
+                    enterGroup();
+                }
+            }
+            catch(Exception e)
+            {
+                var errorMessage = e.Message;
+
+                if(errorMessage == "The remote server returned an error: (400) Bad Request.")
+                {
+                    lb_loggedin.IsVisible = true;
+                    lb_loggedin.Text = "falscher Pincode";
+
+                }
+
+                
+            }
+        }
+
     }
 }
